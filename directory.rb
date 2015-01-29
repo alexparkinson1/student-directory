6493e8355ccf3f7ee22309f5b883478a2a73c85f
Alex Parkinson <aporkinson@gmail.com>
2015-01-29 10:57:14 +0000

    End of tutorial

--- directory.rb
+++ directory.rb
@@ -16,17 +16,17 @@ def print_footer
 end
 
 def input_students
-	puts "please enter the names of the students"
+	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 	# Get the first name 
-	name = gets.chomp
+	name = STDIN.gets.chomp
 	# While the name is not empty, repeat this code
 	while !name.empty? do 
 	  # Add the student hash to the array
-	  @students << {:name => name, :cohort => :february}
+	  add_student(name, :november)
 	  puts "Now we have #{@students.length} students"
 	  # Get another name from the user
-	  name = gets.chomp
+	  name = STDIN.gets.chomp
 	end
 end
 
@@ -38,6 +38,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students
+	when "4"
+		load_students
 	when "9"
 		exit
 	else
@@ -48,7 +50,7 @@ end
 def interactive_menu
 	loop do 
 		print_menu
-		process(gets.chomp)
+		process(STDIN.gets.chomp)
 	end
 end
 
@@ -62,6 +64,7 @@ def print_menu
 	puts "1. Input the students."
 	puts "2. Show the students."
 	puts "3. Save the list to students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit."
 end
 
@@ -77,6 +80,32 @@ def save_students
 	file.close
 end
 
+def add_student(name, cohort)
+	@students << {:name => name, :cohort => cohort.to_sym}
+end
 
 
+def try_load_students
+	filename = ARGV.first # first argument from the command line.
+	return if filename.nil? # get out of the method if it isnt given
+	if File.exists?(filename) # if it exists
+		load_students(filename)
+		puts "Loaded #{@students.length} from #{filename}"
+	else # If it doesnt exist..
+		puts "Sorry, #{filename} doesnt exist."
+		exit # Quit
+	end
+end
+
+
+def load_students(filename = "students.csv")
+	file = File.open(filename, "r")
+	file.readlines.each do |line|
+		name, cohort = line.chomp.split(',')
+		add_student(name, cohort)
+	end
+	file.close
+end
+
+try_load_students
 interactive_menu
