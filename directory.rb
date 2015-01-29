7e0f50f3034ee1086ba6d16089accd27f8a1f0d2
Alex Parkinson <aporkinson@gmail.com>
2015-01-29 10:08:13 +0000

    Refactoring code

--- directory.rb
+++ directory.rb
@@ -3,37 +3,64 @@ def print_header
 	puts "-------------"
 end
 
-def print(students)
-	students.each do |student|
+@students = []
+
+def print_students_list
+	@students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
-def print_footer(names)
-	puts "Overall, we have #{names.length} great students."
+def print_footer
+	puts "Overall, we have #{@students.length} great students."
 end
 
 def input_students
 	puts "please enter the names of the students"
 	puts "To finish, just hit return twice"
-	# Create an empty array 
-	students = []
 	# Get the first name 
 	name = gets.chomp
 	# While the name is not empty, repeat this code
 	while !name.empty? do 
 	  # Add the student hash to the array
-	  students << {:name => name, :cohort => :february}
-	  puts "Now we have #{students.length} students"
+	  @students << {:name => name, :cohort => :february}
+	  puts "Now we have #{@students.length} students"
 	  # Get another name from the user
 	  name = gets.chomp
 	end
-	# Return the array of students
-	students
-end 
+end
 
-# Nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def process(selection)
+	case selection
+	when "1"
+		input_students
+	when "2"
+		show_students
+	when "9"
+		exit
+	else
+		puts "I don't know what you mean by that, please try again."
+	end
+end
+
+def interactive_menu
+	loop do 
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def print_menu
+	puts "1. Input the students."
+	puts "2. Show the students."
+	puts "9. Exit."
+end
+
+
+interactive_menu
